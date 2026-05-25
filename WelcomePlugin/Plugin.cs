c35175448dc033d6e4db8260f224adff67111282
jopinsk <ronalsoda475@gmail.com>
2026-05-25 17:47:37 +0300

    Fix: RoundStarted event has no args in LabAPI 1.1.6

--- WelcomePlugin/Plugin.cs
+++ WelcomePlugin/Plugin.cs
@@ -159,7 +159,7 @@ namespace WelcomePlugin
             }
         }
 
-        private void OnRoundStarted(RoundStartedEventArgs ev)
+        private void OnRoundStarted()
         {
             int counted = 0;
             foreach (Player p in Player.List)
